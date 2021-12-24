7e420940ccb6e11a44d6fbd62fae11263c338c31
Fred Sharapov <f.sharapov@yahoo.com>
2021-12-23 21:39:38 -0500

    sort_array_by_parity

---
+++ Python Course/Learning Python/LeetCode Exercises/sort_array_by_parity.py
@@ -0,0 +1,20 @@
+def sort_array_by_parity(nums):
+    # first we need to add condition where we we check if 
+    # array has less than 0 elements
+    if nums[0] == 0 & len(nums) == 1:
+        return [0]
+    
+    # The optimal solution in here to use two way pointer 
+    # first pointer would be i
+    i = 0
+    # second pointer would be j in loop
+    for j in range(len(nums)):
+        if nums[j] % 2 == 0:
+            nums[i], nums[j] = nums[j], nums[i]
+            i += 1
+    return nums
+
+
+example_test = [0,2]
+print(sort_array_by_parity(example_test))
+
