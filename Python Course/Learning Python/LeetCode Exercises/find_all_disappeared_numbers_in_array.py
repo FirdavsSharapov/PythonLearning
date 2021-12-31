090310214e179499ac4d4074d9f1623381fe2292
Fred Sharapov <f.sharapov@yahoo.com>
2021-12-30 20:03:46 -0500

    fill_all_disappeared_numbers problem from leetcode

--- Python Course/Learning Python/LeetCode Exercises/find_all_disappeared_numbers_in_array.py
+++ Python Course/Learning Python/LeetCode Exercises/find_all_disappeared_numbers_in_array.py
@@ -1,16 +1,29 @@
 def find_disappeared_numbers(nums):
-    i = 1
     holder = []
-    if len(nums) > 1:
-        while i < len(nums):
-            if i not in nums:
-                holder.append(i)
-            i += 1
-        return holder
-    return nums
+        # mark existing numbers 
+    for i in nums:
+        position = abs(i) - 1
+        if nums[position] > 0:
+            nums[position] *= -1
+            
+        for i in range(len(nums)):
+            if nums[i] > 0:
+                holder.append(i+1)
+        
+        
+    return holder
+    # i = 1
+    # holder = []
+    # if len(nums) > 1:
+    #     while i <= len(nums):
+    #         if i not in nums:
+    #             holder.append(i)
+    #         i += 1
+    #     return holder
+    # return nums
     
-
-
 if __name__ == '__main__':
     # assert find_disappeared_numbers([4,3,2,7,8,2,3,1]) == [5,6]
     assert find_disappeared_numbers([1,1]) == [2]
+
+
