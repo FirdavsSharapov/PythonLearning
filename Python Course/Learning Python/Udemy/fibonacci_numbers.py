63bba681dcbda9675f64620d4800cf784911dcf1
Fred Sharapov <f.sharapov@yahoo.com>
2022-03-10 09:41:30 -0500

    modified fibo

--- Python Course/Learning Python/Udemy/fibonacci_numbers.py
+++ Python Course/Learning Python/Udemy/fibonacci_numbers.py
@@ -1,1 +1,9 @@
-def 
+def fibonacci_numbers (num):
+    assert ()
+    if num <= 1:
+        return num
+    else:
+        return fibonacci_numbers(num-1) + fibonacci_numbers(num-2)
+
+
+print(fibonacci_numbers(-1))
